b9553aca356c71f5bf4f539785f073aad28cfe96
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2019-01-28 18:26:49 -0800

    Update facet_scatter_plot.py

--- seaborn/facet_scatter_plot.py
+++ seaborn/facet_scatter_plot.py
@@ -6,7 +6,6 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
-
 r = np.linspace(0, 10, num=100)
 df = pd.DataFrame({'r':r, 'slow':r, 'medium':2, 'fast':4*r})
 df = pd.melt(df, id_vars=['r'], var_name='speed', value_name='theta')
